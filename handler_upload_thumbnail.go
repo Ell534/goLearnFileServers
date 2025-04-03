0b1568dd5206e2870661f97370cef4dbb1c2e93f
Ell534 <elliot94b@gmail.com>
2025-04-03 19:31:51 +0100

    complete CH1L5 complete handlerUploadThumbnail function to allow for upload of thumbnail images and store them in the videoThumbnails map

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,57 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to parse form file", err)
+		return
+	}
+
+	defer file.Close()
+	fileMediaType := header.Header.Get("Content-Type")
+	if fileMediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "missing Content-Type for thumbnail", nil)
+		return
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to read file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		if userID != video.UserID {
+			respondWithError(w, http.StatusUnauthorized, "user is not the video owner", err)
+			return
+		}
+		respondWithError(w, http.StatusBadRequest, "unable to retrieve video metadata", err)
+		return
+	}
+
+	videoThumbnail := thumbnail{
+		data:      data,
+		mediaType: fileMediaType,
+	}
+
+	videoThumbnails[videoID] = videoThumbnail
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		delete(videoThumbnails, videoID)
+		respondWithError(w, http.StatusBadRequest, "unable to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
