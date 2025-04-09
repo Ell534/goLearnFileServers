71547af2aef4f16cabad260d231febf28a20372e
Ell534 <elliot94b@gmail.com>
2025-04-09 16:41:47 +0100

    complete CH2L5 update thumbnail upload handler to give each thumbnail a new path on disk

--- assets.go
+++ assets.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -16,9 +16,14 @@ func (cfg apiConfig) ensureAssetsDir() error {
 	return nil
 }
 
-func getAssetPath(videoID uuid.UUID, mediaType string) string {
+func getAssetPath(mediaType string) string {
+	fileNameBase := make([]byte, 32)
+	rand.Read(fileNameBase)
+
+	fileName := base64.RawURLEncoding.EncodeToString(fileNameBase)
+
 	ext := mediaTypeToExt(mediaType)
-	return fmt.Sprintf("%s%s", videoID, ext)
+	return fmt.Sprintf("%s%s", fileName, ext)
 }
 
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
